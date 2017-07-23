33fcaa8d3ea7e05f18a59bb93a8921322e4ba8e0
Bhawna Budhiraja <Bhawna_Budhiraja@intuit.com>
2017-07-23 12:38:20 +0530

    First commit

---
+++ HibernateFromDetatchedToPersistant/src/com/org/HibernateSession/Main.java
@@ -0,0 +1,18 @@
+package com.org.HibernateSession;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.org.HibernateSession.Student;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Configuration config=new
+		
+	}
+
+}
